978b9462eacd35df457d7417b76405b393b1b5d6
nazma98 <nazmasarker1311@gmail.com>
2025-01-11 00:42:59 +0600

    Add switch case example

--- github.com/nazma98/first-program/main.go
+++ github.com/nazma98/first-program/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	age := 20
+	age := 18
 
 	if age > 18 {
 		fmt.Println("You are eligible to marry")
@@ -11,5 +11,18 @@ func main() {
 		fmt.Println("You are not eligible to marry")
 	} else {
 		fmt.Println("Oh! Just a teenager")
+	}
+
+	b := 30
+
+	switch b {
+	case 1:
+		fmt.Println("b is 1")
+	case 2, 3:
+		fmt.Println("b is 2 or 3")
+	case 4, 5:
+		fmt.Println("b is 4 or 5")
+	default:
+		fmt.Println("b is neither 1 nor 2 nor 3 nor 4 nor 5")
 	}
 }
