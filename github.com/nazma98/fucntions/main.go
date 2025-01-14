3bd221f87a1c93bfd63f186928964663c2d4db1c
nazma98 <nazmasarker1311@gmail.com>
2025-01-14 19:59:59 +0600

    Add functions

--- github.com/nazma98/fucntions/main.go
+++ github.com/nazma98/fucntions/main.go
@@ -11,6 +11,26 @@ func sayHello(name string) {
 }
 
 func main() {
-	printSomething()
-	sayHello("Nazma")
+	// print welcome message
+	fmt.Println("Welcome to the application")
+
+	// Get user name as input
+	var name string
+	fmt.Println("Enter your name -- ")
+	fmt.Scanln(&name)
+
+	var num1 int 
+	var num2 int
+	fmt.Println("Enter first number - ")
+	fmt.Scanln(&num1)
+	fmt.Println("Enter second number - ")
+	fmt.Scanln(&num2)
+	sum := num1 + num2
+
+	// Display results
+	fmt.Println("Hello, ", name)
+	fmt.Println("The sum is - ", sum)
+
+	fmt.Println("Thanks for using our applications!")
+	fmt.Println("Goodbye")
 }
