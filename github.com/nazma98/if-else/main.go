3153838d14ac43453fb795e89c715a2498948f97
nazma98 <nazmasarker1311@gmail.com>
2025-01-16 00:16:53 +0600

    Add functions for single responsibility

--- github.com/nazma98/if-else/main.go
+++ github.com/nazma98/if-else/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+func getNumber() int {
+	var num int
+	fmt.Println("Enter the number - ")
+	fmt.Scanln(&num)
+	return num
+}
+
+func print(result string) {
+	fmt.Println(result)
+}
+
 func findOddEven(num int) string {
 	if(num % 2 == 1) {
 		return "odd"
@@ -10,8 +21,7 @@ func findOddEven(num int) string {
 }
 
 func main() {
-	var num int
-	fmt.Println("Enter the number - ")
-	fmt.Scanln(&num)
-	fmt.Println(findOddEven(num))
+	num := getNumber()
+	result := findOddEven(num)
+	print(result)
 }
