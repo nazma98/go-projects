6b6eaf334cb94a128fb745e75d063069fec3750b
nazma98 <nazmasarker1311@gmail.com>
2025-01-23 00:02:02 +0600

    Add print result

--- github.com/nazma98/if-else/larger-num/main.go
+++ github.com/nazma98/if-else/larger-num/main.go
@@ -5,16 +5,23 @@ import "fmt"
 func getNumbers() (int, int){
 	var num1 int
 	var num2 int
+	fmt.Println("Enter first number : ")
 	fmt.Scanln(&num1)
+	fmt.Println("Enter first number : ")
 	fmt.Scanln(&num2)
 	return num1, num2
 }
 
 func findLargeNumber(num1 int, num2 int) int {
 	max := num1
-	if num2 > max
+	if num2 > max {
+		max = num2
+	}
+	return max
 }
 
 func main() {
 	num1, num2 := getNumbers()
+	max := findLargeNumber(num1, num2)
+	fmt.Println("Large number : ", max)
 }
