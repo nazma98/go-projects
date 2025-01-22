7336ca17f8c148722e841cc704be6d52ebe55f0d
nazma98 <nazmasarker1311@gmail.com>
2025-01-22 23:57:48 +0600

    Add input number method

--- github.com/nazma98/if-else/larger-num/main.go
+++ github.com/nazma98/if-else/larger-num/main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
-func main() {
+func getNumbers() (int, int){
 	var num1 int
 	var num2 int
 	fmt.Scanln(&num1)
 	fmt.Scanln(&num2)
+	return num1, num2
+}
+
+func main() {
+	getNumbers()
 }
