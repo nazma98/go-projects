2669df226ce7756c2102a30f8715c2370b5bd80c
nazma98 <nazmasarker1311@gmail.com>
2025-02-11 00:21:41 +0600

    Add higher order function for rectangle measurement

---
+++ github.com/nazma98/quiz/task2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "math/rand"
+
+func rectangleMethods(length, width float64, op func(x, y float64) float64) {
+	return op(length, width)
+}
+
+func rectangleArea(length, width float64) float64 {
+    return length * width
+}
+
+func rectanglePerimeter(length, width float64) float64 {
+    return 2 * (length + width)
+}
+
+func main() {
+    length := 10.5
+    width := 5.2
+
+    area = rectangleMethods(length, width, rectangleArea)
+    perimeter = rectangleMethods(length, width, rectanglePerimeter)
+
+    fmt.Println("Rectangle Area:", area)
+    fmt.Println("Rectangle Perimeter:", parimeter)
+}
