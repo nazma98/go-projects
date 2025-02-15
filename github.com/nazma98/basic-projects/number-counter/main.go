a6ec462e5008b17b5c10d1a61a44eb6150489425
nazma98 <nazmasarker1311@gmail.com>
2025-02-16 00:04:47 +0600

    Add number of times value function

--- github.com/nazma98/basic-projects/number-counter/main.go
+++ github.com/nazma98/basic-projects/number-counter/main.go
@@ -10,9 +10,21 @@ func getNumber() int {
 }
 
 func getIncrementValue() int {
-	
+	fmt.Println("Enter the increment value : ")
+	var incrementValue int
+	fmt.Scanln(&incrementValue)
+	return incrementValue
+}
+
+func getNumberOfTimes() int {
+	fmt.Println("Enter the number of times to increase : ")
+	var noOfTimes int
+	fmt.Scanln(&noOfTimes)
+	return noOfTimes
 }
 
 func main() {
 	startNumber := getNumber()
+	increment := getIncrementValue()
+	noOfTimes := getNumberOfTimes()
 }
