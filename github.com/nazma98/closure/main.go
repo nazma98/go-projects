441a62a9c11f92e8b231fbadaee2827bbf88c3e5
nazma98 <nazmasarker1311@gmail.com>
2025-02-25 23:10:01 +0600

    Add higher order function outer

--- github.com/nazma98/closure/main.go
+++ github.com/nazma98/closure/main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+const a = 10
+
+var (
+	p = 100
+)
+
+func outer() func() {
+	money := 100
+	age := 30
+	fmt.Println("Age ", age)
+	show := func() {
+		money = money + a + p
+		fmt.Println(money)
+	}
+	return show
+}
+
 func main() {
-	
+
 }
