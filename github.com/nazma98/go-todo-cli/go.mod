39125af7e7aea19e18d26d764a84d658329086b1
nazma98 <nazmasarker1311@gmail.com>
2025-02-27 10:02:26 +0600

    Add todo cli app choices

---
+++ github.com/nazma98/go-todo-cli/go.mod
@@ -0,0 +1,3 @@
+module github.com/nazma98/go-todo-cli
+
+go 1.24.0
---
+++ github.com/nazma98/go-todo-cli/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	// "os"
+)
+
+func main(){
+	fmt.Println("Welcome to TODO app!")
+	fmt.Println("1. Add Task")
+	fmt.Println("2. View Task")
+	fmt.Println("3. Edit Task")
+	fmt.Println("4. Mark Task as Done")
+	fmt.Println("5. Delete Task")
+	fmt.Println("6. Exit")
+}
