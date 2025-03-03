13f754d7284d42afa08487600d1ad8c66aefdbc3
nazma98 <nazmasarker1311@gmail.com>
2025-03-03 23:50:57 +0600

    Instantiate user type object

--- github.com/nazma98/struct/main.go
+++ github.com/nazma98/struct/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 type User struct{
-	Name string,
+	Name string
 	Age int
 }
 
 func main() {
+	var user1 User
 
+	user1 = User {
+		Name : "Nazma",
+		Age : 20,
+	}
+
+	fmt.Println("Name : ", user1.Name)
+	fmt.Println("Age : ", user1.Age)
 }
