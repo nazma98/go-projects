6c7c7705b65a02df50997ec50f13cb61a95a5d17
nazma98 <nazmasarker1311@gmail.com>
2025-03-27 13:28:33 +0600

    Print array

--- github.com/nazma98/pointer/main.go
+++ github.com/nazma98/pointer/main.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+func print(numbers [3]int) {
+	fmt.Println(numbers)
+}
+
 func main() {
 	a := 20
 
 	p := &a
 	fmt.Println(a)
 	fmt.Println(p)
+	fmt.Println(*p)
+
+	arr := [3]int{1, 3, 5}
+	print(arr)
 }
