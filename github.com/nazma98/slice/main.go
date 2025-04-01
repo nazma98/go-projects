8a276feaa0000f09fb193debf196086f8b16d940
nazma98 <nazmasarker1311@gmail.com>
2025-04-01 22:49:56 +0600

    Print slice of array

--- github.com/nazma98/slice/main.go
+++ github.com/nazma98/slice/main.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	arr := [6]string{"This", "is", "a", "Go", "Interview", "question"}
 	fmt.Println(arr)
+
+	s := arr[1:4]
+	fmt.Println(s)
+	fmt.Println(len(s), " ", cap(s))
 }
