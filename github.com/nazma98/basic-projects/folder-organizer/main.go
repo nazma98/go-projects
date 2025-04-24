0c38adf561ee62ff57bb38c2754967e5df9e9532
nazma98 <nazmasarker1311@gmail.com>
2025-04-24 11:44:10 +0600

    Read the directory and list all the files and folders inside it
    - Close the directory after processing everything

--- github.com/nazma98/basic-projects/folder-organizer/main.go
+++ github.com/nazma98/basic-projects/folder-organizer/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	 "fmt"
+	 "os"
+)
 
 func main() {
 	fmt.Println("📁 Folder Organizer Starting...")
+
+	f, err := os.Open("/home/nazma/Documents/go-projects/github.com/nazma98/basic-projects/folder-organizer")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    files, err := f.ReadDir(0)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    for _, v := range files {
+        fmt.Println(v.Name(), v.IsDir())
+    }
+
+	fmt.Println("Closing the directory 📁")
+	defer f.Close()
 }
