49fce654e925866fdd470a12520bb0e4e3c5aa45
nazma98 <nazmasarker1311@gmail.com>
2025-04-24 13:17:26 +0600

    Create folders and move files to respective folders

--- github.com/nazma98/basic-projects/folder-organizer/main.go
+++ github.com/nazma98/basic-projects/folder-organizer/main.go
@@ -7,10 +7,14 @@ import (
 )
 
 func main() {
+	// defaultFolders := []string{"Music", "Videos", "Docs", "Images", "Others"}
+
 	fmt.Println("📁 Folder Organizer Starting...")
 	fmt.Println("")
 
-	f, err := os.Open("/home/nazma/Documents/go-projects/github.com/nazma98/basic-projects/folder-organizer/sample-testing")
+	basePath := "/home/nazma/Documents/go-projects/github.com/nazma98/basic-projects/folder-organizer/sample-testing"
+
+	f, err := os.Open(basePath)
     if err != nil {
         fmt.Println(err)
         return
@@ -24,9 +28,31 @@ func main() {
     for _, v := range files {
 		if !v.IsDir() {
 			ext := filepath.Ext(v.Name())
+			oldPath := filepath.Join(basePath, v.Name())
 			fmt.Println(v.Name(), ext)
+			if ext == ".pdf" || ext == ".doc" {
+				folderName := "Documents"
+				folderPath := filepath.Join(basePath, folderName)
+				newPath := filepath.Join(folderPath, v.Name())
+
+				if info, err := os.Stat(folderPath); err == nil && info.IsDir() {
+						err := os.Rename(oldPath, newPath)
+						if err != nil {
+							fmt.Println("Error moving file:", err)
+							continue
+						}
+						fmt.Printf("📄 %s moving to %s", v.Name(), folderName)
+					} else if os.IsNotExist(err) {
+					err := os.Mkdir(folderPath, 0755)
+					if err != nil {
+						fmt.Println("Error creating folder:", err)
+						return
+					}
+				}
+				} 
+			}
+
 		}
-    }
 
 	fmt.Println("")
 	fmt.Println("🛑 Closing the directory 📁")
