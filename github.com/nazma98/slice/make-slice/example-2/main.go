098e5f7931e65537420be70f6d5515382326fbca
nazma98 <nazmasarker1311@gmail.com>
2025-08-04 14:22:32 +0600

    Add Address of the Backing Array of Both Slices

--- github.com/nazma98/slice/make-slice/example-2/main.go
+++ github.com/nazma98/slice/make-slice/example-2/main.go
@@ -11,5 +11,11 @@ func main() {
 	fmt.Println(len(slice2), cap(slice2))
 	fmt.Println(slice[:])
 	fmt.Println(len(slice), cap(slice))
+	// Print addresses of slice headers (slice variables themselves)
+	fmt.Printf("Address of slice header : %p\n", &slice)
+	fmt.Printf("Address of slice2 header: %p\n", &slice2)
 
+	// Print addresses of underlying data
+	fmt.Printf("Address of slice[0]     : %p\n", &slice[0])
+	fmt.Printf("Address of slice2[0]    : %p\n", &slice2[0])
 }
