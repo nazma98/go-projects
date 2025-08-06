1e4471a111e4661223613481b13dfbcfeea60202
nazma98 <nazmasarker1311@gmail.com>
2025-08-06 23:31:50 +0600

    Add constant example code for quiz

---
+++ github.com/nazma98/constants/quiz-1/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	const PI = 3.1416
+	var a int = 6
+	fmt.Println(a * PI)
+}
---
+++ github.com/nazma98/constants/quiz-2/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	const PI float64
+	PI = 3.1416
+	var a = 6.8
+	fmt.Println(a * PI)
+}
